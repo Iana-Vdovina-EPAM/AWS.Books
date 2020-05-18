a06917b64198356f002c39a9ef8def6fefb248b3
Iana Vdovina <iana_vdovina@epam.com>
2020-05-18 16:15:19 +0400

    Implement CRUD API for book

---
+++ AWS.Books.API/Books.API/Database/IBookDal.cs
@@ -0,0 +1,17 @@
+﻿using Books.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Books.API.Database
+{
+	public interface IBookDal
+	{
+		Task<Book> GetBook(string isbn);
+		Task CreateBook(Book book);
+		Task UpdateBook(Book book);
+
+		Task DeleteBook(string isbn);
+	}
+}
